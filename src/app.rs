b0dbb95bdc27321776d611e51f6cbc59c8994062
Isse <isidornie@gmail.com>
2023-07-25 16:52:09 +0200

    more site

--- src/app.rs
+++ src/app.rs
@@ -1,27 +1,98 @@
+use strum::{Display, EnumIter, IntoEnumIterator};
 use yew::prelude::*;
+use yew_router::prelude::*;
 
-pub enum Msg {}
+#[derive(Clone, Routable, PartialEq, Display)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/projects")]
+    Projects,
+    #[at("/about")]
+    About,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
 
-#[derive(Debug, Default)]
-pub struct App {}
+fn switch(route: Route) -> Html {
+    render_page(&route, || match route {
+        Route::Home => html! { <Home /> },
+        Route::Projects => html! { <Projects /> },
+        Route::About => html! { <About /> },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    })
+}
 
-impl Component for App {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self::default()
+#[function_component]
+pub fn App() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
+}
 
-    fn update(&mut self, _ctx: &Context<Self>, _msg: Self::Message) -> bool {
-        true
-    }
+fn render_page(route: &Route, render: impl Fn() -> Html) -> Html {
+    let selectable = [Route::Home, Route::Projects, Route::About];
+    let selectable_count = selectable.len();
+    let page_buttons = selectable
+        .into_iter()
+        .enumerate()
+        .map(|(i, r)| {
+            let first_last = if i == 0 {
+                "rounded-bl-md"
+            } else if i == selectable_count - 1 {
+                "rounded-br-md"
+            } else {
+                ""
+            };
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {
-        html! {
-            <main>
-                <h1>{"Work in Progress 🦀🦀🦀"}</h1>
-            </main>
-        }
+            if r == *route {
+                let class = format!("px-5 py-2 bg-sky-600 {first_last}");
+                html! {
+                    <span class={class}> {r} </span>
+                }
+            } else {
+                let class = format!("px-5 py-2 bg-zinc-800 hover:bg-zinc-600 {first_last}");
+                let name = r.to_string();
+                html! {
+                    <Link<Route> to={r}> <span class={class}> {name} </span> </Link<Route>>
+                }
+            }
+        })
+        .collect::<Html>();
+
+    html! {
+        <main>
+            <div class="bg-black text-white h-screen">
+            <div class="flex justify-between w-full">
+                <h1 class="text-3xl pl-3 pt-1">{"Isse"}</h1>
+                <div class="font-semibold select-none">
+                    {page_buttons}
+                </div>
+                <a href="https://github.com/IsseW/site" target="_blank"
+                    class="select-none px-5 py-2 font-semibold rounded-bl-md bg-purple-600 hover:bg-purple-500">
+                    {"<> source"}
+                </a>
+            </div>
+            {render()}
+            </div>
+        </main>
     }
+}
+
+#[function_component]
+fn Home() -> Html {
+    html! { "Hello World!" }
+}
+
+#[function_component]
+fn Projects() -> Html {
+    html! { "My projects" }
+}
+
+#[function_component]
+fn About() -> Html {
+    html! { "I am Isse" }
 }
