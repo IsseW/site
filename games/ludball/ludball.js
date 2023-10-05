8227e896890114400e6bc902b7719e57e39e88a4
Isse <isidornie@gmail.com>
2023-10-05 13:23:41 +0200

    fix links

--- games/ludball/ludball.js
+++ games/ludball/ludball.js
@@ -1895,7 +1895,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('ludball.wasm', import.meta.url);
+        input = new URL('./ludball.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
